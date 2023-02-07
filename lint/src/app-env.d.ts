c6560974eda398a66248ac432b83c7ddf181461e
saiki naoya <naoya.saiki@rightcode.co.jp>
2023-02-07 14:16:02 +0900

    lint,formatter

---
+++ lint/src/app-env.d.ts
@@ -0,0 +1,10 @@
+/// <reference types ="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_APP_TITLE: string;
+  // ここに定義した変数を追加
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
